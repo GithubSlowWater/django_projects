7de8e60839ee4737e76724fe849e201c2f685100
GithubSlowWater <568033067@qq.com>
2019-01-17 22:11:36 +0800

    机构详情页逻辑完成

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -3,11 +3,17 @@ __author__ = 'JK'
 __date__ = '2019/1/16 21:34'
 
 from django.conf.urls import url, include
-from .views import OrglistView, AddUserAskView, OrgHomeView
+from .views import OrglistView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, AddFavView
 
 urlpatterns = [
     #课程机构首页
     url(r'^list/$', OrglistView.as_view(), name="org_list"),
     url(r'^add_ask', AddUserAskView.as_view(), name="add_ask"),
     url(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name="org_home"),
+    url(r'^course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name="org_course"),
+    url(r'^desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name="org_desc"),
+    url(r'^teachers/(?P<org_id>\d+)/$', AddFavView.as_view(), name="org_teachers"),
+
+    # 机构收藏
+    url(r'^add_fav', AddUserAskView.as_view(), name="add_fav"),
 ]
